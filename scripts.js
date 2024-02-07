2e7e74569de994a19289b61a974d30b7a6501635
Melissa <melissabsouzaa@gmail.com>
2024-02-07 12:10:02 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function alerta(){
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    alert("seus dados são:\n" + "email: "+ email +"\nsenha: " + senha);
+}
